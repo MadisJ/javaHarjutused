4ff15915d90d1ea5fd57a1b137dacb17e724b74c
Madis Järvpõld <mjarvpol@klass1-19-l.intra.itcollege.ee>
2015-10-11 14:34:10 +0300

    Ristmik

--- src/teema1/Peamurdja2_ristmik.java
+++ src/teema1/Peamurdja2_ristmik.java
@@ -1,5 +1,6 @@
 package teema1;
 
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import lib.Foor;
@@ -10,35 +11,48 @@ import lib.Foor;
  */
 public class Peamurdja2_ristmik extends Application {
     public void start(Stage primaryStage) throws Exception {
-        Foor foor = new Foor(Foor.YLEMINE,primaryStage);
-        Foor foor1 = new Foor(Foor.ALUMINE,primaryStage);
-        Foor foor2 = new Foor(Foor.VASAK,primaryStage);
+        Foor foor1 = new Foor(Foor.YLEMINE, primaryStage);
+        Foor foor2 = new Foor(Foor.ALUMINE, primaryStage);
+        Foor foor3 = new Foor(Foor.VASAK, primaryStage);
+        Foor foor4 = new Foor(Foor.PAREM, primaryStage);
+        tsykkel(foor1, 0);
+        tsykkel(foor2, 0);
+        tsykkel(foor3, 1);
+        tsykkel(foor4, 1);
 
-        int i = 0;
-        int k = 0;
 
-        while (k < 5) {
-            foor.vahetaPunast();
-            foor.paus(3);
-            foor.vahetaKollast();
-            foor.paus(1);
-            foor.vahetaRohelist();
-            foor.vahetaKollast();
-            foor.vahetaPunast();
-            foor.paus(3);
-            while (i < 11) {
-                foor.vahetaRohelist();
-                foor.paus(0.3);
-                i = i + 1;
-            }
-            foor.vahetaKollast();
-            foor.paus(1);
-            foor.vahetaKollast();
-            k = k + 1;
-            i=0;
-        }
-        foor.vahetaPunast();
-        foor.paus(2);
-        foor.vahetaPunast();
     }
-}
+    public void tsykkel (Foor foor, int suund) {
+        int i = 0;
+        int  k = suund;
+        while (k < 5) {
+            if (k != 0) {
+
+                foor.vahetaPunast();
+                foor.paus(9);
+                foor.vahetaKollast();
+                foor.paus(1);
+                foor.vahetaKollast();
+                foor.vahetaPunast();
+            }
+                foor.vahetaRohelist();
+                foor.paus(3);
+                while (i < 11) {
+                    foor.vahetaRohelist();
+                    foor.paus(0.3);
+                    i = i + 1;
+                }
+                foor.vahetaKollast();
+                foor.paus(1);
+                foor.vahetaKollast();
+                k = k + 1;
+                i = 0;
+            }
+            foor.vahetaPunast();
+            foor.paus(2);
+            foor.vahetaPunast();
+
+        }
+    }
+
+
